26b76b17fda9c3e63aef87e9276cfa3446ef9ea8
Bhautik <bhirpara600@gmail.com>
2025-08-05 14:10:27 +0530

    First commit and project setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import { FaCartPlus } from "react-icons/fa";
+
+function Navbar() {
+  return (
+    <>
+      <nav className='bg-[#0D1B2A] '>
+        <section className="flex items-center justify-between py-4 px-8 mx-auto big:max-w-[1536px]">
+          <div className='text-3xl font-bold flex items-center'>
+            <p className='text-[#FF6F00]'>Apana</p><span className='text-[#fff]'>Bazaar</span>
+          </div>
+          <ul className='text-[#FFF8E1] flex items-center space-x-4 text-lg font-medium'>
+            <li><a href="#">Home</a></li>
+            <li className="text-2xl relative">
+              <a href="#"><FaCartPlus /><span className="text-[12px] bg-red-500 text-center size-4 rounded-full absolute top-[-10px] right-0">2</span></a>
+            </li>
+          </ul>
+        </section>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
